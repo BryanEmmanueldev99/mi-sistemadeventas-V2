ef92e14cd7bed2db4b6042746838452d530f123c
Bryan el chico de mkt <emmanuelbryan1999@gmail.com>
2024-12-10 16:41:53 -0600

    reiniciamos el proyecto

---
+++ database/migrations/2024_12_10_222940_create_clientes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('razon_social', 255);
+            $table->string('celular', 255)->nullable();
+            $table->string('email_cliente', 255)->unique()->nullable();
+            $table->string('nit_ci_cliente', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
---
+++ database/migrations/2024_12_10_223413_create_ventas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nro_venta', 255);
+            $table->double('cambio')->nullable();
+            $table->double('total_pagado');
+            $table->string('folio', 255);
+            $table->foreignId('cliente_id')->constrained('clientes')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+};
